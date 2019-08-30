7a4434bba2a279d0eb92cdea5c3792458b3e9d04
IsaiasLeos <ialeos@miners.utep.edu>
2019-08-29 19:00:00 -0600

    better comments + adding words to dictionary

--- wordCount.py
+++ wordCount.py
@@ -2,29 +2,37 @@ import sys        # command line arguments
 import re         # regular expression tools
 import os         # checking if file exists
 
-# Checks the amount of argument variables given when executing this program.
+# Checks the amount of argument variables given.
 if len(sys.argv) is not 3:
     print("Usage: wordCount.py <input> <output>")
     exit()
 
-# Assigns the following variables to the input and output files given when executing this program.
+# Save argument to variables.
 inputName = sys.argv[1]
 outputName = sys.argv[2]
 wordDictionary = {}
 
-# Checks if the following output file exists
+# Checks if the following output.txt file exists
 if not os.path.exists(outputName):
     print("Output %s file doesn't exist! Terminating..." % outputName)
     exit()
 
-with open(inputName, 'r') as input:  # Read the file as assign its value inside of words
+with open(inputName, 'r') as input:  # Read the file
     for line in input:  # Assign each line from 'input' into the variable 'line'
-        
-        # Remove commas, periods, and new lines and also make sentence lowercase
-        line = re.sub('[,.\n]', '', line).lower()
 
-        # Split the 'line' variable by blank spaces and assign it into the variable 'split'
+        # Remove commas, periods, and new lines
+        line = re.sub('[,.\n]', '', line)
+        line = line.lower()  # make sentence lowercase
+        
+        # Split the 'line' variable by blank spaces
         line = line.split(' ')
-        
 
+        # Iterate through each line and check if the word is in the dictionary
+        for word in line:
+            # Increase word count in dictionary
+            if word in wordDictionary:
+                wordDictionary[word] += 1
+            else:
+                # Add word to dictionary
+                wordDictionary[word] = 1
 print(wordDictionary)
