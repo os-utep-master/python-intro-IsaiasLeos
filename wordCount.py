e8341124993c97c0c5ba0b76a1c3ebbff1d6a5de
IsaiasLeos <ialeos@miners.utep.edu>
2019-08-29 18:55:12 -0600

    bugfixes + remove punctuation

--- wordCount.py
+++ wordCount.py
@@ -2,22 +2,25 @@ import sys        # command line arguments
 import re         # regular expression tools
 import os         # checking if file exists
 
-
-#Checks the amount of argument variables given when executing this program.
+# Checks the amount of argument variables given when executing this program.
 if len(sys.argv) is not 3:
     print("Usage: wordCount.py <input> <output>")
     exit()
 
-#Assigns the following variables to the input and output files given when executing this program.
+# Assigns the following variables to the input and output files given when executing this program.
 inputName = sys.argv[1]
 outputName = sys.argv[2]
-words = {}
+wordDictionary = {}
 
-#Checks if the following output file exists
+# Checks if the following output file exists
 if not os.path.exists(outputName):
     print("Output %s file doesn't exist! Terminating..." % outputName)
     exit()
 
-with open(inputName,'r') as words:
-    for line in words:
-        print(line)
+with open(inputName, 'r') as input:  # Read the file as assign its value inside of words
+    for line in input:  # Assign each line from 'input' into the variable 'line'
+        
+        # Remove commas, periods, and new lines and also make sentence lowercase
+        line = re.sub('[,.\n]', '', line).lower()
+        
+print(wordDictionary)
